a3e3fe84ab1ee2d36a73f9a82f16d630983b01f2
roflmaostc <fxw+git@mailbox.org>
2021-11-23 10:39:13 -0500

    Add more tests

--- test/datatype_tools.jl
+++ test/datatype_tools.jl
@@ -9,6 +9,13 @@
 
 
     @testset "Test default_type" begin
+        Ts = [Bool, Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128]
+        
+        for T in Ts
+            @test NDTools.default_type(T, Float16) == Float16
+        end
+
+
         @test NDTools.default_type(Int32, Float32) == Float32
         @test NDTools.default_type(Float64, Float32) == Float64
         @test NDTools.default_type(ComplexF64, Float32) == ComplexF64 
@@ -25,5 +32,7 @@
         @test get_complex_datatype(22.2) == ComplexF64
         @test get_complex_datatype(22.2f0) == ComplexF32
         @test get_complex_datatype(22) == Complex{Int64}
+        @test get_complex_datatype(1f0 + 1f0 * 1im) == Complex{Float32}
+        @test get_complex_datatype([1f0 + 1f0 * 1im]) ==Complex{Float32}  
     end
 
