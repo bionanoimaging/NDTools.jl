e1449c33c0eaa9d5b8558f513e830756831523b0
RainerHeintzmann <heintzmann@gmail.com>
2025-02-11 18:32:44 +0100

    changed array datatype for select_region_view since to make it work for CUDA, but MutablePaddedView is not compatible

--- src/MutablePaddedViews.jl
+++ src/MutablePaddedViews.jl
@@ -9,12 +9,18 @@ Base.size(A::MutablePaddedView) = size(A.data)
 
 # The change below makes select_region into a writable region
 Base.@propagate_inbounds function Base.setindex!(A::MutablePaddedView{T,N}, v, pos::Vararg{Int,N}) where {T,N}
+    @show pos
     @boundscheck checkbounds(A.data, pos...)
     if Base.checkbounds(Bool, A.data.data, pos...)
         return setindex!(A.data.data, v, pos... )
     else
         return A.data.fillvalue
     end
+end
+
+function Base.fill!(A::SubArray{T,N,MutablePaddedView{T,N}, R, B}, x) where {T,N,R,B}
+    @show size(A)
+    @show t
 end
 
 Base.@propagate_inbounds function Base.getindex(A::MutablePaddedView{T,N}, pos::Vararg{Int,N}) where {T,N}
